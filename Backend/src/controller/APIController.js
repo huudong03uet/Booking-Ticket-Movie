d9f7ed5954c89dfc01136e6090fc97158930fa8c
nguyenvankhang <21020768@vnu.edu.vn>
2023-03-20 23:00:39 +0700

    sửa 1 tí :v

--- Backend/src/controller/APIController.js
+++ Backend/src/controller/APIController.js
@@ -1,7 +1,7 @@
 import pool from '../configs/connectDB.js'
 
 let getNowPlaying = async (req, res) => {
-    const [rows, fields] = await pool.execute('SELECT * FROM `film` WHERE now() > dates_minium and now() < dates_maxium;'); 
+    const [rows, fields] = await pool.execute('SELECT title, rate, (SELECT COUNT(*) FROM ticket t INNER JOIN showtime s on t.showtime_id = s.showtime_id WHERE s.film_id = film.film_id) as popularity, genres, release_date, film_id, poster FROM film WHERE now() < dates_minium and now() > dates_maxium;'); 
     return res.status(200).json({
         message: "Get now playing film",
         data: rows

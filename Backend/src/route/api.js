bd36b1883f3b0e3e947e40d88d5fc8bd551d998b
nguyenvankhang <21020768@vnu.edu.vn>
2023-03-20 22:38:46 +0700

    api getplaying film

--- Backend/src/route/api.js
+++ Backend/src/route/api.js
@@ -4,10 +4,10 @@ let router = express.Router();
 
 const initAPIRoute = (app) => {
   
-    router.get('/users', APIController.getAllUsers);// method GET => read data
-    router.post('/create-user', APIController.createNewUser);
-    router.put('/update-user', APIController.updateUser);
-    router.delete('/delete-user/:id', APIController.deleteUser);
+    router.get('/nowPlaying', APIController.getNowPlaying);// method GET => read data
+    // router.post('/create-user', APIController.createNewUser);
+    // router.put('/update-user', APIController.updateUser);
+    // router.delete('/delete-user/:id', APIController.deleteUser);
 
     return app.use('/api/v1/', router);
 }

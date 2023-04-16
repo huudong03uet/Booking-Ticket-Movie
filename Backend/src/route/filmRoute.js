d0f379815a9d014f1aad0102f11e9784ecc72f72
nguyenvankhang <21020768@vnu.edu.vn>
2023-04-16 16:41:03 +0700

    sua ti

--- Backend/src/route/filmRoute.js
+++ Backend/src/route/filmRoute.js
@@ -1,15 +1,15 @@
 import express from 'express';
-import APIController from '../controller/filmController.js';
+import filmController from '../controller/filmController.js';
 let router = express.Router();
 
 const initAPIRoute = (app) => {
   
-    router.get('/nowPlaying', APIController.getNowPlaying);// method GET => read data
-    router.get('/filmComing', APIController.getUpComing);
-    router.get('/filmPopular', APIController.getFilmPopular);
-    router.get('/topRated', APIController.getTopRated);
-    router.get('/trailer/:id', APIController.getTrailer);
-    router.get('/schedule/:id', APIController.getSchedule);
+    router.get('/nowPlaying', filmController.getNowPlaying);// method GET => read data
+    router.get('/filmComing', filmController.getUpComing);
+    router.get('/filmPopular', filmController.getFilmPopular);
+    router.get('/topRated', filmController.getTopRated);
+    router.get('/trailer/:id', filmController.getTrailer);
+    router.get('/schedule/:id', filmController.getSchedule);
     return app.use('/api/v1/', router);
 }
 
